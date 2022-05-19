1ee583057185f503ccffbb32bf9ea0889f04b441
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-05-19 14:06:41 +0700

    tugas 4 js
    tugas 4 js - niomic

---
+++ tugas4.js
@@ -0,0 +1,17 @@
+function tinggiBadan() {
+    var x = 170;
+    var y = 160;
+    var z = 155;
+
+    if (x>y) {
+        console.log(x, "Tertinggi")
+    }
+    if (y>z) {
+        console.log(y, "Normal")
+    }
+    if(z<y) {
+        console.log(z, "Terendah")
+    }
+}
+
+tinggiBadan()
